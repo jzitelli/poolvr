e102be134c45f1804d0b29a96bbad1db96d51056
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2015-12-22 21:19:20 -0500

    asdfasdf

--- test/run_tests.py
+++ test/run_tests.py
@@ -11,27 +11,6 @@ from pyserver.flask_app import app, render_template, Markup, main, json, request
 
 from test_client import POOLVRTest, ConfiguratorTest
 
-# from three import *
-
-# @app.route('/test')
-# def _test_page():
-#     scene = Scene()
-#     scene.add(Mesh(geometry=PlaneBufferGeometry(width=8, height=8),
-#                    material=MeshBasicMaterial(color=0x5555ff),
-#                    position=[0, -2, -4],
-#                    rotation=[-np.pi/2, 0, 0],
-#                    cannonData={'mass': 0, 'shapes': ['Plane']}))
-#     return render_template('test.html',
-#                            links=Markup('\n<br>\n'.join([r"<a href='%s'>%s</a>" % (endpoint, endpoint)
-#                                                          for endpoint in ('test/cannon',
-#                                                                           'test/heightfield',
-#                                                                           'test/skybox')])),
-#                            json_config=Markup(r"""<script>
-# var THREE_PY_CONFIG = %s;
-# var JSON_SCENE = %s;
-# </script>""" % (json.dumps({'controls': request.args.get('controls')}, indent=2),
-#                 json.dumps(scene.export(), indent=2))))
-
 
 if __name__ == "__main__":
     import logging
---
+++ test/run_tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./run_tests.py --with-needle-capture --with-needle-cleanup-on-success -x
