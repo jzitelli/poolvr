f7971302f352099b004a559ed026f336268f4f25
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-02-16 13:38:01 -0500

    update gh-pages build

--- poolvr-0.1.0dev/build/poolvr.js
+++ poolvr-0.1.0dev/build/poolvr.js
@@ -1338,7 +1338,7 @@ POOLVR.playCollisionSound = (function () {
     var ballBallBuffer;
     var request = new XMLHttpRequest();
     request.responseType = 'arraybuffer';
-    request.open('GET', '/sounds/ballBall.ogg');
+    request.open('GET', 'sounds/ballBall.ogg');
     request.onload = function() {
         WebVRSound.audioContext.decodeAudioData(this.response, function(buffer) {
             ballBallBuffer = buffer;
@@ -1357,7 +1357,7 @@ POOLVR.playPocketedSound = (function () {
     var ballPocketedBuffer;
     var request = new XMLHttpRequest();
     request.responseType = 'arraybuffer';
-    request.open('GET', '/sounds/ballPocketed.ogg');
+    request.open('GET', 'sounds/ballPocketed.ogg');
     request.onload = function() {
         WebVRSound.audioContext.decodeAudioData(this.response, function(buffer) {
             ballPocketedBuffer = buffer;
