e7ca6c4e4b2aa796ede78b84334c5934d9644104
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-06-01 06:46:05 -0400

    style

--- src/sounds.js
+++ src/sounds.js
@@ -3,7 +3,7 @@ POOLVR.playCollisionSound = (function () {
     "use strict";
     var ballBallBuffer;
     var filename = (!/Edge/.test(navigator.userAgent)) ? 'sounds/ballBall.ogg' : 'sounds/ballBall.mp3';
-    YAWVRB.Audio.loadBuffer(filename, function(buffer) {
+    YAWVRB.Audio.loadBuffer(filename, function (buffer) {
         ballBallBuffer = buffer;
     });
     var playCollisionSound = function (v) {
@@ -16,7 +16,7 @@ POOLVR.playPocketedSound = (function () {
     "use strict";
     var ballPocketedBuffer;
     var filename = (!/Edge/.test(navigator.userAgent)) ? 'sounds/ballPocketed.ogg' : 'sounds/ballPocketed.mp3';
-    YAWVRB.Audio.loadBuffer(filename, function(buffer) {
+    YAWVRB.Audio.loadBuffer(filename, function (buffer) {
         ballPocketedBuffer = buffer;
     });
     var playPocketedSound = function () {
