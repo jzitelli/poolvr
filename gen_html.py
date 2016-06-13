0e5e32c77d3dc7458274e2c9c41e5e33943e06cf
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-06-13 00:16:52 -0400

    dist script now all python

--- gen_html.py
+++ gen_html.py
@@ -1,18 +1,21 @@
 import json
+import os
+import shutil
 from jinja2 import Environment, FileSystemLoader, Markup
 
 from poolvr import TEMPLATE_FOLDER, WebVRConfig, POOLVR, pool_table, GIT_REVS
 
 
-OUTPUT = 'dist/poolvr.html'
+OUTPUT_DIR = 'dist'
 
 
 def main():
+    shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+    shutil.copytree('build', os.path.join(OUTPUT_DIR, 'build'))
     env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER))
     template = env.get_template('poolvr_template.html')
-    # POOLVR['config']['toolOptions']['port'] = 6438 # for hosting on gh-pages (https)
     s = template.render(config={'DEBUG': False},
-   	                    json_config=Markup(r"""<script>
+                        json_config=Markup(r"""<script>
 var WebVRConfig = %s;
 
 var POOLVR = %s;
@@ -21,7 +24,7 @@ var THREEPY_SCENE = %s;
 </script>""" % (json.dumps(WebVRConfig, indent=2),
                 json.dumps(POOLVR, indent=2),
                 json.dumps(pool_table.pool_hall(**POOLVR['config']).export()))),
-   	                    version_content=Markup(r"""
+                        version_content=Markup(r"""
 <h3>v{2}</h3>
 <table>
 <tr>
@@ -34,10 +37,30 @@ var THREEPY_SCENE = %s;
 </tr>
 </table>
 """.format(GIT_REVS[0], GIT_REVS[1], POOLVR['version'])))
-    with open(OUTPUT, 'w') as f:
-    	f.write(s)
-
-
+    with open(os.path.join(OUTPUT_DIR, 'poolvr.html'), 'w') as f:
+        f.write(s)
+    # copy resources:
+    shutil.copy('poolvr.css', OUTPUT_DIR)
+    shutil.copy('favicon.ico', OUTPUT_DIR)
+    shutil.copytree('fonts', os.path.join(OUTPUT_DIR, 'fonts'))
+    shutil.copytree('images', os.path.join(OUTPUT_DIR, 'images'))
+    shutil.copytree('sounds', os.path.join(OUTPUT_DIR, 'sounds'))
+    # copy dependencies:
+    # os.mkdirs(os.path.join(OUTPUT_DIR, 'node_modules', 'cannon', 'build'))
+    # shutil.copy(os.path.join('node_modules', 'cannon', 'build', 'cannon.min.js'), os.path.join(OUTPUT_DIR, 'node_modules', 'cannon', 'build'))
+    shutil.copytree(os.path.join('node_modules', 'cannon', 'build'), os.path.join(OUTPUT_DIR, 'node_modules', 'cannon', 'build'))
+    os.makedirs(os.path.join(OUTPUT_DIR, 'node_modules', 'leapjs'))
+    shutil.copy(os.path.join('node_modules', 'leapjs', 'leap-0.6.4.min.js'), os.path.join(OUTPUT_DIR, 'node_modules', 'leapjs'))
+    os.makedirs(os.path.join(OUTPUT_DIR, 'node_modules', 'three.js', 'build'))
+    os.makedirs(os.path.join(OUTPUT_DIR, 'node_modules', 'three.js', 'examples'))
+    shutil.copy(os.path.join('node_modules', 'three.js', 'build', 'three.min.js'), os.path.join(OUTPUT_DIR, 'node_modules', 'three.js', 'build'))
+    shutil.copytree(os.path.join('node_modules', 'three.js', 'examples', 'js'), os.path.join(os.path.join(OUTPUT_DIR, 'node_modules', 'three.js', 'examples', 'js')))
+    os.makedirs(os.path.join(OUTPUT_DIR, 'node_modules', 'three.py'))
+    shutil.copytree(os.path.join('node_modules', 'three.py', 'js'), os.path.join(OUTPUT_DIR, 'node_modules', 'three.py', 'js'))
+    os.makedirs(os.path.join(OUTPUT_DIR, 'node_modules', 'webvr-polyfill', 'build'))
+    shutil.copy(os.path.join('node_modules', 'webvr-polyfill', 'build', 'webvr-polyfill.js'), os.path.join(OUTPUT_DIR, 'node_modules', 'webvr-polyfill', 'build'))
+    os.makedirs(os.path.join(OUTPUT_DIR, 'node_modules', 'yawvrb', 'build'))
+    shutil.copy(os.path.join('node_modules', 'yawvrb', 'build', 'yawvrb.js'), os.path.join(OUTPUT_DIR, 'node_modules', 'yawvrb', 'build'))
 
 if __name__ == "__main__":
 	main()
