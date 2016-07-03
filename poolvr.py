92cff255fc2a8446e99c985cafba5e00862bf4d2
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-07-03 04:25:35 -0400

    rm dist.py

--- poolvr.py
+++ poolvr.py
@@ -5,18 +5,23 @@ import os
 import logging
 _logger = logging.getLogger(__name__)
 import json
+import os
+import shutil
+import sys
+import subprocess
 from copy import deepcopy
 
-import subprocess
+from flask import Flask, render_template, request, Markup
+from jinja2 import Environment, FileSystemLoader
+
+import pool_table
+
 
 completed_proc = subprocess.run(['git', 'rev-list', '--max-count=4', 'HEAD'], stdout=subprocess.PIPE, check=True, universal_newlines=True)
 GIT_REVS = []
 for line in completed_proc.stdout.splitlines():
     GIT_REVS.append(line)
 
-from flask import Flask, render_template, request, Markup
-
-import pool_table
 
 STATIC_FOLDER   = os.path.abspath(os.path.split(__file__)[0])
 TEMPLATE_FOLDER = STATIC_FOLDER
@@ -29,6 +34,9 @@ app = Flask(__name__,
             template_folder=TEMPLATE_FOLDER)
 
 app.debug = True
+
+env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER))
+template = env.get_template('poolvr_template.html')
 
 
 WebVRConfig = {
@@ -58,8 +66,6 @@ POOLVR = {
         'ball_diameter'      : 2.25 * pool_table.INCH2METER,
         'H_ceiling'          : 8 * 12 * 0.0254,
         'synthSpeakerVolume' : 0.5,
-        'synthSpeakerRate'   : 0.7,
-        'synthSpeakerPitch'  : 0.5,
         'toolOptions': {
             'tipShape'               : 'Cylinder',
             'numSegments'            : 8,
@@ -74,27 +80,6 @@ POOLVR = {
         }
     }
 }
-
-
-def get_poolvr_config():
-    """
-    Constructs poolvr config dict based on request url parameters.
-    """
-    config = deepcopy(POOLVR['config'])
-    args = dict({k: v for k, v in request.args.items()
-                 if k in config})
-    for k, v in args.items():
-        if v == 'false':
-            args[k] = False
-        elif v == 'true':
-            args[k] = True
-        elif not (v is False or v is True or v is None):
-            try:
-                args[k] = float(v)
-            except Exception as err:
-                pass
-    config.update(args)
-    return config
 
 
 def get_webvr_config():
@@ -118,16 +103,34 @@ def get_webvr_config():
     return config
 
 
+def get_poolvr_config():
+    """
+    Constructs poolvr config dict based on request url parameters.
+    """
+    config = deepcopy(POOLVR['config'])
+    args = dict({k: v for k, v in request.args.items()
+                 if k in config})
+    for k, v in args.items():
+        if v == 'false':
+            args[k] = False
+        elif v == 'true':
+            args[k] = True
+        elif not (v is None):
+            try:
+                args[k] = float(v)
+            except Exception as err:
+                pass
+    config.update(args)
+    return {'config': config,
+            'version': POOLVR['version']}
 
-@app.route('/')
-def poolvr():
-    """
-    Serves the poolvr app HTML.
-    """
-    poolvr_config = get_poolvr_config()
-    pool_hall = pool_table.pool_hall(**poolvr_config)
-    webvr_config = get_webvr_config()
-    return render_template("poolvr_template.html",
+
+def render_poolvr_template(webvr_config=None, poolvr_config=None):
+    if webvr_config is None:
+        webvr_config = WebVRConfig
+    if poolvr_config is None:
+        poolvr_config = POOLVR
+    return template.render(config={'DEBUG': app.debug},
                            json_config=Markup(r"""<script>
 var WebVRConfig = %s;
 
@@ -135,15 +138,64 @@ var POOLVR = %s;
 
 var THREEPY_SCENE = %s;
 </script>""" % (json.dumps(webvr_config, indent=2),
-                json.dumps({'version': POOLVR['version'],
-                            'config': poolvr_config}, indent=2),
-                json.dumps(pool_hall.export()))),
+                json.dumps(poolvr_config, indent=2),
+                json.dumps(pool_table.pool_hall(**POOLVR['config']).export()))),
                            version_content=Markup(r"""
 <h3>v{2}</h3>
+<table>
+<tr>
+<td>
 <a href="https://github.com/jzitelli/poolvr/commit/{0}">current commit</a>
-<br>
+</td>
+<td>
 <a href="https://github.com/jzitelli/poolvr/commit/{1}">previous commit</a>
+</td>
+</tr>
+</table>
 """.format(GIT_REVS[0], GIT_REVS[1], POOLVR['version'])))
+
+
+@app.route('/')
+def poolvr():
+    """
+    Serves the poolvr app HTML.
+    """
+    webvr_config = get_webvr_config()
+    poolvr_config = get_poolvr_config()
+    pool_hall = pool_table.pool_hall(**poolvr_config)
+    return render_poolvr_template(webvr_config=webvr_config, poolvr_config=poolvr_config)
+
+
+DIST_OUTPUT_DIR = 'dist'
+
+def make_dist():
+    shutil.rmtree(DIST_OUTPUT_DIR, ignore_errors=True)
+    shutil.copytree('build', os.path.join(DIST_OUTPUT_DIR, 'build'))
+    with open(os.path.join(DIST_OUTPUT_DIR, 'poolvr.html'), 'w') as f:
+        f.write(render_poolvr_template())
+    # copy resources:
+    shutil.copy('poolvr.css', DIST_OUTPUT_DIR)
+    shutil.copy('favicon.ico', DIST_OUTPUT_DIR)
+    shutil.copytree('fonts', os.path.join(DIST_OUTPUT_DIR, 'fonts'))
+    shutil.copytree('images', os.path.join(DIST_OUTPUT_DIR, 'images'))
+    shutil.copytree('sounds', os.path.join(DIST_OUTPUT_DIR, 'sounds'))
+    # copy npm dependencies:
+    shutil.copytree(os.path.join('node_modules', 'cannon', 'build'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'cannon', 'build'))
+    os.makedirs(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'leapjs'))
+    shutil.copy(os.path.join('node_modules', 'leapjs', 'leap-0.6.4.min.js'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'leapjs'))
+    os.makedirs(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'build'))
+    shutil.copy(os.path.join('node_modules', 'three.js', 'build', 'three.js'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'build'))
+    shutil.copy(os.path.join('node_modules', 'three.js', 'build', 'three.min.js'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'build'))
+    os.makedirs(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'examples'))
+    shutil.copytree(os.path.join('node_modules', 'three.js', 'examples', 'js'), os.path.join(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'examples', 'js')))
+    shutil.copytree(os.path.join('node_modules', 'three.js', 'examples', 'models'), os.path.join(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'examples', 'models')))
+    shutil.copytree(os.path.join('node_modules', 'three.js', 'examples', 'textures'), os.path.join(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.js', 'examples', 'textures')))
+    os.makedirs(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.py'))
+    shutil.copytree(os.path.join('node_modules', 'three.py', 'js'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'three.py', 'js'))
+    os.makedirs(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'webvr-polyfill', 'build'))
+    shutil.copy(os.path.join('node_modules', 'webvr-polyfill', 'build', 'webvr-polyfill.js'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'webvr-polyfill', 'build'))
+    os.makedirs(os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'yawvrb', 'build'))
+    shutil.copy(os.path.join('node_modules', 'yawvrb', 'build', 'yawvrb.js'), os.path.join(DIST_OUTPUT_DIR, 'node_modules', 'yawvrb', 'build'))
 
 
 
@@ -168,7 +220,11 @@ def main():
 
 
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=(logging.DEBUG if app.debug else logging.INFO),
                         format="%(asctime)s %(levelname)s %(name)s %(funcName)s %(lineno)d:  %(message)s")
-    main()
+    if len(sys.argv) == 2 and sys.argv[1] == 'dist':
+        make_dist()
+    else:
+        main()
